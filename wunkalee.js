9941a58a8daf7b2feaf6f9cd7839128328f8d1c1
Luvboy-76 <abvcxz12345678966@gmail.com>
2021-02-02 18:11:44 +0800

    Update wunkalee.js

--- wunkalee.js
+++ wunkalee.js
@@ -40,17 +40,6 @@
     navSlide();
     
     
-    //google api
-
-    let map;
-
-      function initMap() {
-        map = new google.maps.Map(document.getElementById("geo-map"), {
-          center: { lat: -34.397, lng: 150.644 },
-          zoom: 8,
-        });
-      }
-    
     
 
 })();
